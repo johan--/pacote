8bd9b59f891b05410f9b874a52849bb27152a065
Johan Pretorius <johan.pretorius@gmail.com>
2018-12-06 09:52:10 +0200

    first run

--- formio.config.ts
+++ formio.config.ts
@@ -2,10 +2,10 @@ import { FormioAppConfig } from 'angular-formio';
 // import { FormioAuthConfig } from 'angular-formio/auth';
 
 export const AppConfig: FormioAppConfig = {
-  // appUrl: 'https://nnglnkqdvbdjltz.form.io',
-  // apiUrl: 'https://api.form.io',
-  appUrl: 'http://localhost:3001',
-  apiUrl: 'http://localhost:3001',
+  appUrl: 'https://nnglnkqdvbdjltz.form.io',
+  apiUrl: 'https://api.form.io',
+  // appUrl: 'http://localhost:3001',
+  // apiUrl: 'http://localhost:3001',
   icons: 'fontawesome'
 };
 
